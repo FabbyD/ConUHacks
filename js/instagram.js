9b4efb0ae04594f1c11ae1fc3d3f4c1b5f1d1b5d
Fabrice Dugas <fabrice.dugas@polymtl.ca>
2016-01-23 23:43:32 -0500

    tSAD

--- js/instagram.js
+++ js/instagram.js
@@ -1,18 +1,14 @@
-var accessToken = '2739012414.2768f83.34d827d9701447c08c5fc596f6668f6c';
-
 $.ajax({
-    url: 'https://api.instagram.com/v1/media/popular',
     dataType: 'jsonp',
     type: 'GET',
-	cache: false,
     url: "https://api.instagram.com/v1/media/popular?access_token=2739012414.2768f83.34d827d9701447c08c5fc596f6668f6c",
     success: function(data){
-        console.log(data);
-        for(x in data.data){
-          $(".popular").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></li>");  
-        }
+        console.log('pute');
+        //for(x in data.data){
+        //  $(".popular").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></li>");  
+        //}
     },
     error: function(data){
-        console.log(data);
+        console.log('plotte');
     }
 });

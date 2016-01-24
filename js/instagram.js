f4f88247389ffe991165536342f6d032678e98f6
Fabrice Dugas <fabrice.dugas@polymtl.ca>
2016-01-23 23:01:57 -0500

    instagram de shit

--- js/instagram.js
+++ js/instagram.js
@@ -1,10 +1,14 @@
 function callInsta() {
 	console.log('Instafeed');	
-	var feed = new Instafeed({
-			limit: 5,
-			clientId: 'd3eb5b70c04b4356a146d2cf73988aff',
-			accessToken: '2739012414.d3eb5b7.c6aaf652d65d4b3e838c233deace95a7'
-		});
-	
-	feed.run();
+	 $.ajax({
+      type: "GET",
+      dataType: "jsonp",
+      cache: false,
+      url: "https://api.instagram.com/v1/media/popular?access_token=2739012414.2768f83.34d827d9701447c08c5fc596f6668f6c",
+      success: function(data) {
+        for (var i = 0; i < 6; i++) {
+          $(".popular").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a></li>");
+		}
+      }
+     })
 }

0ed50d10220e09368ca2a1d0b07441c2b9545822
Fabrice Dugas <fabrice.dugas@polymtl.ca>
2016-01-23 23:13:37 -0500

    instagram ajax essaie dawg

--- js/instagram.js
+++ js/instagram.js
@@ -1,14 +1,17 @@
-function callInsta() {
-	console.log('Instafeed');	
-	 $.ajax({
-      type: "GET",
-      dataType: "jsonp",
-      cache: false,
-      url: "https://api.instagram.com/v1/media/popular?access_token=2739012414.2768f83.34d827d9701447c08c5fc596f6668f6c",
-      success: function(data) {
-        for (var i = 0; i < 6; i++) {
-          $(".popular").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a></li>");
-		}
-      }
-     })
-}
+var accessToken = '2739012414.2768f83.34d827d9701447c08c5fc596f6668f6c';
+
+$.ajax({
+    url: 'https://api.instagram.com/v1/media/popular',
+    dataType: 'jsonp',
+    type: 'GET',
+    url: "https://api.instagram.com/v1/media/popular?access_token=2739012414.2768f83.34d827d9701447c08c5fc596f6668f6c",
+    success: function(data){
+        console.log(data);
+        for(x in data.data){
+          $('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');  
+        }
+    },
+    error: function(data){
+        console.log(data);
+    }
+});

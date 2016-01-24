8d93fa73ae59bbdd60806542b202a546a786fdf0
Pierre-Yves Lajoie <pierre-yves.lajoie@polymtl.ca>
2016-01-23 20:16:04 -0500

    ajout du chemin sur la carte

--- js/map.js
+++ js/map.js
@@ -14,11 +14,14 @@ var map;
 var marker;
 function initMap() {
     var johnMolson = {lat: 45.495261, lng: -73.578760};
+	var directionsService = new google.maps.DirectionsService;
+	var directionsDisplay = new google.maps.DirectionsRenderer;
     map = new google.maps.Map(document.getElementById('contact'), {
         center: johnMolson,
         zoom: 15,
 		scrollwheel: false
     });
+	directionsDisplay.setMap(map);
 
     marker = new google.maps.Marker({
         position: johnMolson,
@@ -42,4 +45,20 @@ function onPlaceChanged() {
   } else {
     document.getElementById('autocomplete').placeholder = 'Enter your destination';
   }
+  determineRoute(directionsService, directionsDisplay);
+}
+
+
+function determineRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: johnMolson,
+    destination: place.location,
+    travelMode: google.maps.TravelMode.DRIVING
+  }, function(response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
 }

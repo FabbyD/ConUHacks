b01cf8a47d3825ba649a41a323f4ec000a1a9624
Fabrice Dugas <fabrice.dugas@polymtl.ca>
2016-01-23 23:19:37 -0500

    besoin d'essayer sur gh-pages

--- js/instagram.js
+++ js/instagram.js
@@ -4,11 +4,12 @@ $.ajax({
     url: 'https://api.instagram.com/v1/media/popular',
     dataType: 'jsonp',
     type: 'GET',
+	cache: false,
     url: "https://api.instagram.com/v1/media/popular?access_token=2739012414.2768f83.34d827d9701447c08c5fc596f6668f6c",
     success: function(data){
         console.log(data);
         for(x in data.data){
-          $('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');  
+          $(".popular").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></li>");  
         }
     },
     error: function(data){
